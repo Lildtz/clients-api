dcb9435ea94ea41a80bff980610e6d4ee8f390c9
Levartoski <a.levartoski@gmail.com>
2022-08-22 20:18:57 -0300

    cliente / servico rep

--- src/main/java/io/github/lildtz/clientsapi/ClientsApiApplication.java
+++ src/main/java/io/github/lildtz/clientsapi/ClientsApiApplication.java
@@ -1,11 +1,15 @@
 package io.github.lildtz.clientsapi;
 
+import io.github.lildtz.clientsapi.model.entity.Cliente;
+import io.github.lildtz.clientsapi.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientsApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClientsApiApplication.class, args);
 	}
---
+++ src/main/java/io/github/lildtz/clientsapi/rest/ClienteController.java
@@ -0,0 +1,57 @@
+package io.github.lildtz.clientsapi.rest;
+
+import io.github.lildtz.clientsapi.model.entity.Cliente;
+import io.github.lildtz.clientsapi.model.repository.ClienteRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteController(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(@RequestBody @Valid Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente findById(@PathVariable Integer id) {
+        return clienteRepository.findById(id).orElseThrow(
+                ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "Cliente não encontrado!")
+        );
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar( @PathVariable Integer id) {
+        clienteRepository.findById(id).map( cliente -> {
+            clienteRepository.delete(cliente);
+            return Void.TYPE;
+        }).orElseThrow(
+                ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Cliente não encontrado!")
+        );
+    }
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar( @PathVariable Integer id, @RequestBody @Valid Cliente clienteAtualizado) {
+        clienteRepository.findById(id).map( cliente -> {
+            clienteAtualizado.setId(cliente.getId());
+            return clienteRepository.save(clienteAtualizado);
+        }).orElseThrow(
+                ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Cliente não encontrado!")
+        );
+    }
+}
