0eec5df74dab82a352230a5f7e4ea3c36c26c11f
Levartoski <a.levartoski@gmail.com>
2022-08-23 23:05:54 -0300

    Cliente getAll

--- src/main/java/io/github/lildtz/clientsapi/rest/ClienteController.java
+++ src/main/java/io/github/lildtz/clientsapi/rest/ClienteController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 
     private final ClienteRepository clienteRepository;
@@ -18,6 +20,10 @@ public class ClienteController {
         this.clienteRepository = clienteRepository;
     }
 
+    @GetMapping
+    public List<Cliente> getAll(){
+        return clienteRepository.findAll();
+    }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar(@RequestBody @Valid Cliente cliente) {
